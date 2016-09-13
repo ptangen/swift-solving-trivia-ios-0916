27344dfc9f6e3a533a5e6eb592e7bec89d959f28
Paul Tangen <ptangen@ptangen.com>
2016-09-13 12:43:52 -0400

    Complete the solving trivia lab.

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,11 +12,31 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
+    // Problems statement:
+    // What is the only state whose capital contains none of the characters as the state name?
+    // Example: Alaska is not the answer because Juneau and Alaska both contain an A.
+    
+    func solveTrivia(stateCapitals: Dictionary<String, String>) -> String {
+        var stateWithoutCharInCapital: String?
 
+        for state in stateCapitals.keys { // iterate over all the states in the dictionary
+            var capitalCharMatchesStateChar:Bool = false
+            if let capital = stateCapitals[state] { // unwrap the capital
+                for capitalChar in capital.characters { // iterate over the chars in capital
+                    capitalCharMatchesStateChar = state.localizedCaseInsensitiveContainsString(String(capitalChar)) // look for a match
+                    if capitalCharMatchesStateChar {
+                        print("\(capitalChar) from \(capital) was found in \(state).")
+                        break
+                    }
+                }
+                if !capitalCharMatchesStateChar {
+                    // goal met: found a state name where none of the characters match the characters in the capital, return the state name
+                    print("**** No character in \(capital) was found in \(state). ****")
+                    stateWithoutCharInCapital = state
+                }
+            }
+        }
+        return (stateWithoutCharInCapital)!
+    }
 }
 
--- swift-solving-trivia/ViewController.swift
+++ swift-solving-trivia/ViewController.swift
@@ -13,12 +13,67 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let result = AppDelegate().solveTrivia(stateCapitals)
+        print(result)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var stateCapitals = [
+        "Alabama": "Montgomery",
+        "Alaska": "Juneau",
+        "Arizona": "Phoenix",
+        "Arkansas": "Little Rock",
+        "California": "Sacramento",
+        "Colorado": "Denver",
+        "Connecticut": "Hartford",
+        "Delaware": "Dover",
+        "Florida": "Tallahassee",
+        "Georgia": "Atlanta",
+        "Hawaii": "Honolulu",
+        "Idaho": "Boise",
+        "Illinois": "Springfield",
+        "Indiana": "Indianapolis",
+        "Iowa": "Des Moines",
+        "Kansas": "Topeka",
+        "Kentucky": "Frankfort",
+        "Louisiana": "Baton Rouge",
+        "Maine": "Augusta",
+        "Maryland": "Annapolis",
+        "Massachusetts": "Boston",
+        "Michigan": "Lansing",
+        "Minnesota": "Saint Paul",
+        "Mississippi": "Jackson",
+        "Missouri": "Jefferson City",
+        "Montana": "Helena",
+        "Nebraska": "Lincoln",
+        "Nevada": "Carson City",
+        "New Hampshire": "Concord",
+        "New Jersey": "Trenton",
+        "New Mexico": "Santa Fe",
+        "New York": "Albany",
+        "North Carolina": "Raleigh",
+        "North Dakota": "Bismarck",
+        "Ohio": "Columbus",
+        "Oklahoma": "Oklahoma City",
+        "Oregon": "Salem",
+        "Pennsylvania": "Harrisburg",
+        "Rhode Island": "Providence",
+        "South Carolina": "Columbia",
+        "South Dakota": "Pierre",
+        "Tennessee": "Nashville",
+        "Texas": "Austin",
+        "Utah": "Salt Lake City",
+        "Vermont": "Montpelier",
+        "Virginia": "Richmond",
+        "Washington": "Olympia",
+        "West Virginia": "Charleston",
+        "Wisconsin": "Madison",
+        "Wyoming": "Cheyenne"
+    ]
 
 
 }
